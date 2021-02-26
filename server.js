4463932772a8e8a270019c623f940a1a4666c8a7
punitive_1729 <ojha1111pk@gmail.com>
2021-02-26 13:45:07 +0530

    second commit

--- server.js
+++ server.js
@@ -5,10 +5,7 @@ const path = require('path');
 const get__ = require('./Routes/get');
 const down__ = require('./Routes/download');
 const cors = require('cors');
-const cron = require('node-cron');
 const connectDB = require('./configs/db');
-const File = require('./Model/file');
-const fs = require('fs');
 
 // express connection
 const app = express();
@@ -32,26 +29,6 @@ app.use('/api/files', post__);
 app.use('/files', get__);
 
 app.use('/files/download', down__);
-
-cron.schedule('* 3 * * *', async function() {
-    connectDB();
-    // Get all records older than 24 hours 
-    const files = await File.find({ createdAt : { $lt: new Date(Date.now() - 1)} });
-    
-    if(files.length) {
-        for (const file of files) {
-            try {
-                fs.unlinkSync(file.path);
-                await file.remove();
-                console.log(`successfully deleted ${file.filename}`);
-            }catch(err){
-                console.log(`error while deleting file ${err} `);
-            }
-        }
-    }
-    console.log('Job done!');
-});
-
 
 app.listen(PORT, ()=>{
     console.log("listening on port Number", PORT);
